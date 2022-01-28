8a18143585f3a39016583aebf4a1f2cbb21bfa8f
CK-OCSB <97174126+CK-OCSB@users.noreply.github.com>
2022-01-28 13:56:11 +0000

    First bit of code

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
-# DATE OF CREATION:  
-# PURPOSE OF PROGRAM:  
+# NAME OF AUTHOR: Carter King (CK-OCSB)
+# NAME OF THE PROGRAM: Math Practice  
+# DATE OF CREATION: 2022-01-28 ,  
+# PURPOSE OF PROGRAM: To provide a serises of math problems to the user. The user will chose the type of questions as well as the difficulty.  
 
 # VARIABLE DEFINITION
+MathType = str(input("Welcome to Math Practice!\nIn this program you will be given math questions\n You will chose the catagory of your questions as well as the difficulty of your chosing."))
 
+
+import time
+timeAmmount = 
+while timeAmmount :
+    mins = timeAmmount // 60
+    secs = timeAmmount % 60 
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    time.sleep(1)
+    timeAmmount-= 1
+print("timer over")
 # INPUT
+
+
+
 
 # PROCESSING
 
