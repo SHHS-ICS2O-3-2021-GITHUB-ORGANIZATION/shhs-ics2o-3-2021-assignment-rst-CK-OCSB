c0350ad78385d5469d31202573a6ed1903bc1276
CK-OCSB <97174126+CK-OCSB@users.noreply.github.com>
2022-01-30 15:02:21 +0000

    Work done on Jan 29th 2022

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 # INPUT
 print("Welcome to Math Practice! \nIn this program your math skills will be tested! \nYou will chose the category of your questions as well as the difficulty of your choosing.")
 MathChoise = str(input('What kind of math would you like to practice? \nIf you would like to choose addition type "Add" \nIf you would like to chose subtraction type "Sub" \nIf you would like to choose Multiplication type "Multi" \nIf you would like to chose Division type "Div" \nWhat would you like to choose:'))
-
 if MathChoise:
     DiffSet = int(input('The difficulty of for this practice ranges from 1 to 5, \n1 is the the easiest and 5 being the hardest, the harder the difficulty, the less amount of time you will have to answer even harder questions!  \nPlease select a difficulty by typing a number:'))
 
@@ -14,41 +13,20 @@ if MathChoise:
 # VARIABLE DEFINITION
 import time
 import random
+
 #######Time Settings for the difficulty#########
 if DiffSet == 1:
-    TimeVar = 211
+    TimeVar = 210
 if DiffSet == 2:
-    TimeVar =181
+    TimeVar =180
 if DiffSet == 3:
-    TimeVar = 121
+    TimeVar = 120
 if DiffSet == 4:
-    TimeVar = 91
+    TimeVar = 90
 if DiffSet == 5:
     TimeVar = 55
 #Every "TimeVar" has an additional second to give the user the full amount of time
-#################################################
-if DiffSet == 1:
-    AddRange1 = random.randint(1,40)
-    AddRange2 = random.randint(1,40)
-    AddTotal = AddRange1 + AddRange2
-if DiffSet == 2:
-    AddRange1 = random.randint(1,75)
-    AddRange2 = random.randint(1,75)
-    AddTotal = AddRange1 + AddRange2
-if DiffSet == 3:
-    AddRange1 = random.randint(1,100)
-    AddRange2 = random.randint(1,100)
-    AddTotal = AddRange1 + AddRange2
-if DiffSet == 4:
-    AddRange1 = random.randint(1,150)
-    AddRange2 = random.randint(1,150)
-    AddTotal = AddRange1 + AddRange2
-if DiffSet == 5:
-    AddRange1 = random.randint(1,200)
-    AddRange2 = random.randint(1,200)
-    AddTotal = AddRange1 + AddRange2
-    
-###############################################
+###################
 if DiffSet == 1:
     SubRange1 = random.randint(1,20)
     SubRange2 = random.randint(1,25)
@@ -69,7 +47,7 @@ if DiffSet == 5:
     SubRange1 = random.randint(1,100)
     SubRange2 = random.randint(1,85)
     SubTotal = SubRange1 + SubRange2
-    ##########################################
+    #################
 if DiffSet == 1:
     MultiRange1 = random.randint(1,5)
     MultiRange2 = random.randint(1,10)
@@ -90,7 +68,7 @@ if DiffSet == 5:
     MultiRange1 = random.randint(1,15)
     MultiRange2 = random.randint(5,20)
     MultiTotal = MultiRange1 + MultiRange2
-    ############################################
+    ####################
 if DiffSet == 1:
     DivRange1 = random.randint(1,3)
     DivRange2 = random.randint(1,30)
@@ -111,25 +89,80 @@ if DiffSet == 5:
     DivRange1 = random.randint(5,12)
     DivRange2 = random.randint(1,150)
     DivTotal = DivRange1 + DivRange2
-####################################################
-
-    
-    
+#########################
+TimeAmmount = 1 + TimeVar
+QuestionsAnswerd = 0
+CorrectAnswers = 0
+IncorrectAnswers = 0 
+# PROCESSING
 """
-timeAmmount = 
-while timeAmmount :
-    mins = timeAmmount // 60
-    secs = timeAmmount % 60 
+while TimeAmmount :
+    mins = TimeAmmount // 60
+    secs = TimeAmmount % 60 
     timer = '{:02d}:{:02d}'.format(mins, secs)
     time.sleep(1)
-    timeAmmount-= 1
-print("timer over")
-"""""
+    TimeAmmount-= 1
+"""
+###################
 
+if MathChoise == ("Add"):
+  print("You have chosen addition your questions begin NOW!")
 
-# PROCESSING
-  
-  
-  
-  
-# OUTPUT
+while MathChoise == ("Add") and DiffSet == 1:
+  #while TimeAmmount != 0:
+    AddRange1 = random.randint(1,40)
+    AddRange2 = random.randint(1,40)
+    print("Your questions is",AddRange1,"+",AddRange2)
+    UserAnswer = int(input("Your answer is : "))
+    AddTotal = AddRange1 + AddRange2
+    QuestionsAnswerd = QuestionsAnswerd + 1
+    if UserAnswer == AddTotal :
+      CorrectAnswers = CorrectAnswers + 1 
+    if UserAnswer != AddTotal:
+      IncorrectAnswers = IncorrectAnswers + 1
+while MathChoise == ("Add") and DiffSet == 2:
+  #while TimeAmmount != 0:
+    AddRange1 = random.randint(1,75)
+    AddRange2 = random.randint(1,75)
+    print("Your questions is",AddRange1,"+",AddRange2)
+    UserAnswer = int(input("Your answer is : "))
+    AddTotal = AddRange1 + AddRange2
+    QuestionsAnswerd = QuestionsAnswerd + 1
+    if UserAnswer == AddTotal :
+      CorrectAnswers = CorrectAnswers + 1 
+    if UserAnswer != AddTotal:
+      IncorrectAnswers = IncorrectAnswers + 1
+while MathChoise == ("Add") and DiffSet == 3:
+ #while TimeAmmount != 0:
+    AddRange1 = random.randint(1,100)
+    AddRange2 = random.randint(1,100)
+    print("Your questions is",AddRange1,"+",AddRange2)
+    UserAnswer = int(input("Your answer is : "))
+    AddTotal = AddRange1 + AddRange2
+    QuestionsAnswerd = QuestionsAnswerd + 1
+    if UserAnswer == AddTotal :
+      CorrectAnswers = CorrectAnswers + 1 
+    if UserAnswer != AddTotal:
+      IncorrectAnswers = IncorrectAnswers + 1
+while MathChoise == ("Add") and DiffSet == 4:
+  #while TimeAmmount != 0:
+    AddRange1 = random.randint(1,150)
+    AddRange2 = random.randint(1,150)
+    print("Your questions is",AddRange1,"+",AddRange2)
+    UserAnswer = int(input("Your answer is : "))
+    AddTotal = AddRange1 + AddRange2
+    QuestionsAnswerd = QuestionsAnswerd + 1
+    if UserAnswer == AddTotal :
+      CorrectAnswers = CorrectAnswers + 1 
+    if UserAnswer != AddTotal:
+     IncorrectAnswers = IncorrectAnswers + 1
+while MathChoise == ("Add") and DiffSet == 5:
+  #while TimeAmmount != 0:
+    print("Your questions is",random.randint(1,200),"+",random.randint(1,200))
+    UserAnswer = int(input("Your answer is : "))
+    AddTotal = AddRange1 + AddRange2
+    QuestionsAnswerd = QuestionsAnswerd + 1
+    if UserAnswer == AddTotal :
+      CorrectAnswers = CorrectAnswers + 1 
+    if UserAnswer != AddTotal:
+      IncorrectAnswers = IncorrectAnswers + 1
